984547839f0722e5d5a2255b9e412af2a25ef76f
Tomas Diaz <tomydiaz739@gmail.com>
2023-04-26 15:57:14 -0300

    hola mundo

---
+++ src/proyecto_algoritmos/ProyectoAlgoritmos.java
@@ -0,0 +1,11 @@
+package proyecto_algoritmos;
+
+public class ProyectoAlgoritmos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+
+	}
+
+}
