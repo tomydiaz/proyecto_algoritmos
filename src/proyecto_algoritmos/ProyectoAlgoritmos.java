eefdafa1f5386f1859f71ee1773f6ed62e0472c7
manuelrosa <manuel98rosa@gmail.com>
2023-04-27 20:14:21 -0300

    Intento 12

--- src/proyecto_algoritmos/ProyectoAlgoritmos.java
+++ src/proyecto_algoritmos/ProyectoAlgoritmos.java
@@ -4,7 +4,7 @@ public class ProyectoAlgoritmos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Intento 11, 2do con consola");
+		System.out.println("Intento 12, 2do con consola");
 	}
 
 }
