6c0cf13109ea9037191f5e48a40821e031631970
Tomas Diaz <tomydiaz739@gmail.com>
2023-04-26 22:58:42 -0300

    cambio texto

--- src/proyecto_algoritmos/ProyectoAlgoritmos.java
+++ src/proyecto_algoritmos/ProyectoAlgoritmos.java
@@ -4,7 +4,7 @@ public class ProyectoAlgoritmos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo");
+		System.out.println("soy tomas y cambie el texto");
 	}
 
 }
